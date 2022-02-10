bdf75fc7852198613a703c214cd01f9cfb009785
accounts@shawnpatoka.com <accounts@shawnpatoka.com>
2022-02-10 15:44:54 -0600

    finished assignment (I think), and added not found handler for lookup by title.

--- notes_api.py
+++ notes_api.py
@@ -15,13 +15,26 @@ NOTES = {
 
 
 
-# not found handler
+# not found handlers
 
 def abort_if_note_doesnt_exist(note_id):
     if note_id not in NOTES:
         abort(404, message="Note {} doesn't exist".format(note_id))
 
+def abort_if_note_doesnt_exist_by_title(title):
+    titles = []
+    # add note titles to a list
+    for note in NOTES:
+        temp = NOTES[note]
+        titles.append(temp['title'])
+    
+    # search to see if given title is in titles list. If not, raise a 404
+    if title not in titles:
+        abort(404, message="Note '{}' doesn't exist".format(title))
 
+
+
+# setting args
 
 parser = reqparse.RequestParser()
 parser.add_argument('title')
@@ -39,35 +52,49 @@ class Notes(Resource):
         args = parser.parse_args()
         new_note_id = len(NOTES) + 1
         NOTES[new_note_id] = {'title': args['title'], 'body': args['body'], }
-        return NOTES[new_note_id], 201
+        return new_note_id, 201
    
 
 
 class NoteByID(Resource):
     def get(self, id):
         abort_if_note_doesnt_exist(id)
-        return NOTES[id]
+        return NOTES[id], 200
         
+    def put(self, id):
+        abort_if_note_doesnt_exist(id)
+        args = parser.parse_args()
+        note = {'title': args['title'], 'body': args['body'], }
+        NOTES[id] = note
+        return note, 201
 
-        
-    
-    # def put(self, note_id):
-    #     args = parser.parse_args()
-    #     note = {'note': args['note']}
-    #     NOTES[note_id] = note
-    #     return note, 201
+    def delete(self, id):
+        abort_if_note_doesnt_exist(id)
+        del NOTES[id]
+        return "BLAH", 204
 
-    # def delete(self, note_id):
-    #     abort_if_note_doesnt_exist(note_id)
-    #     del NOTES[note_id]
-    #     return '', 204
+
 
 class NoteByTitle(Resource):
     def get(self, title):
-        for item in NOTES:
-            current_note = NOTES[item]
+        abort_if_note_doesnt_exist_by_title(title)
+        for num in NOTES:
+            current_note = NOTES[num]
+            print(current_note)
             if current_note['title'] == title:
-                return current_note
+                return current_note, 200
+
+    def put(self, title):
+        abort_if_note_doesnt_exist_by_title(title)
+        for num in NOTES:
+            current_note = NOTES[num]
+            if current_note['title'] == title:
+                args = parser.parse_args()
+                updated_info = {'title': args['title'], 'body': args['body'], }
+                NOTES[num] = updated_info
+                return NOTES[num], 201
+
+
 
 
 # routes
